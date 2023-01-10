4555984caf4fbe6bf0063eac4f6e8f97517b09ab
jpromanonet <programming@jpromano.net>
2023-01-10 15:18:26 -0300

    Working over navbar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,14 +11,8 @@ export const Footer = () => {
       <Container>
         <Row className="align-items-center">
           <Col size={12} sm={6}>
-            <img src={logo} alt="Logo" />
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
-            <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
-            </div>
             <p>Copyright 2023. All Rights Reserved</p>
           </Col>
         </Row>
