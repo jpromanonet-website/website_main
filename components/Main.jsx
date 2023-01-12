36f750013750f3f76cf88bca45761996979b425b
jpromanonet <programming@jpromano.net>
2023-01-12 14:38:06 -0300

    Main

--- components/Main.jsx
+++ components/Main.jsx
@@ -17,10 +17,9 @@ const Main = () => {
           </h1>
           <h1 className='py-2 text-gray-700'>Engineering Manager & Teacher</h1>
           <p className='py-4 text-gray-600 sm:max-w-[70%] m-auto'>
-            I’m focused on building responsive front-end web applications
-            integrating back-end technologies.
+            I’m focused on teaching people how to code and leading teams in the development of new products, solutions and processes.
           </p>
-          <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
+          <div className='flex items-center justify-center max-w-[330px] m-auto py-4'>
             <a
               href='https://www.linkedin.com/in/jpromanonet/'
               target='_blank'
@@ -39,16 +38,6 @@ const Main = () => {
                 <FaGithub />
               </div>
             </a>
-            <Link href='/#contact'>
-              <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-                <AiOutlineMail />
-              </div>
-            </Link>
-            <Link href='/resume'>
-              <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-                <BsFillPersonLinesFill />
-              </div>
-            </Link>
           </div>
         </div>
       </div>
