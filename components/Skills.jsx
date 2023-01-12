a2fdf9b9e6c19e2562e13619900e2e91a6cd869c
jpromanonet <programming@jpromano.net>
2023-01-12 14:44:15 -0300

    Main

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -99,6 +99,16 @@ const Skills = () => {
               </div>
             </div>
           </div>
+          <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+              <div className='m-auto'>
+                <Image src={AWS} width='64px' height='64px' alt='/' />
+              </div>
+              <div className='flex flex-col items-center justify-center'>
+                <h3>AWS</h3>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
