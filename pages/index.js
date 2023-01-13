0ed9bb8b5ed87d31519f44a93006ca9421501865
jpromanonet <programming@jpromano.net>
2023-01-13 16:27:28 -0300

    No more firebase

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import About from '../components/About'
 import Contact from '../components/Contact'
 import Main from '../components/Main'
-import Projects from '../components/Projects'
 import Skills from '../components/Skills'
 
 
@@ -17,7 +16,6 @@ export default function Home() {
     <Main />
     <About />
     <Skills />
-    <Projects />
     <Contact />
     </div>
   )
