9436b65814ec5bba5bff79244538067846408161
jpromanonet <programming@jpromano.net>
2023-02-09 14:59:53 -0300

    New resume

--- pages/resume.jsx
+++ pages/resume.jsx
@@ -76,7 +76,7 @@ const resume = () => {
             <span className='px-2'>|</span>Django
             <span className='px-2'>|</span>Python
             <span className='px-2'>|</span>Node JS
-            <span className='px-2'>|</span>R
+            <span className='px-2'>|</span>
           </p>
           <p className='py-2'>
             <span className='font-bold'>Amazon Web Services</span>
