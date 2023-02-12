64b97936d02e9005770a4941f11e4fb83a4908ce
jpromanonet <programming@jpromano.net>
2023-02-12 15:33:20 -0300

    Contact updated

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
 import Link from "next/link";
 import React, { useState, useEffect } from "react";
 import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from "react-icons/ai";
 import { FaGithub, FaLinkedinIn } from "react-icons/fa";
-import { BsFillPersonLinesFill } from "react-icons/bs";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
