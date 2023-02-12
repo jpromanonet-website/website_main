3c7aaf278a00b90ddfe15f78a29b8cad74e1712a
jpromanonet <programming@jpromano.net>
2023-02-12 15:46:26 -0300

    Contact updated

--- components/About.jsx
+++ components/About.jsx
@@ -10,22 +10,24 @@ const About = () => {
           <p className="uppercase text-xl tracking-widest text-[#000]">About</p>
           <h2 className="py-4">Who Am I?</h2>
           <p className="py-2 text-gray-600">
-            I specialize in building mobile responsive front-end UI applications
-            that connect with API’s and other backend technologies. I’m
-            passionate about learning new technologies and understand there is
-            more than one way to accomplish a task. Though I am most proficient
-            in building front-end applications using HTML, CSS, Javascript, and
-            React, I am a quick learner and can pick up new tech stacks as
-            needed. I believe that being a great developer is not using one
-            specific language, but choosing the best tool for the job.
+            My love for technology and solving problems started at a young age.
+            I taught myself how to code with a book about Quick Basic and an old
+            IBM PC, and I've been hooked ever since. I've been fortunate enough
+            to turn my passion into a successful career and continue to expand
+            my knowledge and skills.
           </p>
           <p className="py-2 text-gray-600">
-            I started web developement in 2013 managing multiple e-commerce
-            websites on CMS platforms such as WordPress, BigCommerce, and
-            Shopify. I have experience working directly with clients and taking
-            mock wireframes all the way to deployed applications. In my spare
-            time I run Code Commerce, a Youtube channel where I teach web
-            developement and various front-end technologies.
+            In addition to my professional achievements, I have a passion for
+            books and building things. I'm always looking for ways to help
+            others and make a positive impact in their lives. Whether it's
+            through coding, selling websites, or simply lending a hand, I find
+            great joy in helping others.
+          </p>
+          <p className="py-2 text-gray-600">
+            I bring my technical expertise and leadership skills to the table,
+            and I'm excited to join a new team where I can continue making a
+            difference. I'm eager to bring my positive energy and passion for
+            problem-solving to any organization.
           </p>
         </div>
         <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
