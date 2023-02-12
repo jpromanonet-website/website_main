b96971ae8b654004ef7112cc7ac39308b458376c
jpromanonet <programming@jpromano.net>
2023-02-12 02:16:17 -0300

    Contact updated

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,4 +1,6 @@
-{/* Import skill icons */}
+{
+  /* Import skill icons */
+}
 import Image from "next/image";
 import React from "react";
 import Html from "../public/assets/skills/html.png";
@@ -12,15 +14,14 @@ import NextJS from "../public/assets/skills/nextjs.png";
 import AWS from "../public/assets/skills/aws.png";
 import Redux from "../public/assets/skills/redux.svg";
 
-
-{/* Creating skill cards*/}
+{
+  /* Creating skill cards*/
+}
 const Skills = () => {
   return (
     <div id="skills" className="w-full lg:h-screen p-2">
       <div className="max-w-[1240px] mx-auto flex flex-col justify-center h-full">
-        <p className="text-xl tracking-widest uppercase text-[#5651e5]">
-          Skills
-        </p>
+        <p className="text-xl tracking-widest uppercase text-[#000]">Skills</p>
         <h2 className="py-4">What I Can Do</h2>
         <div className="grid grid-cols-2 lg:grid-cols-4 gap-8">
           <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
