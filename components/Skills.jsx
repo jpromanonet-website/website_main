58854fab17d581f0707bb60eb926f83c064e79c2
jpromanonet <programming@jpromano.net>
2023-02-12 00:23:54 -0300

    Contact updated

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,15 +1,19 @@
-import Image from 'next/image';
-import React from 'react';
-import Html from '../public/assets/skills/html.png';
-import Css from '../public/assets/skills/css.png';
-import Javascript from '../public/assets/skills/javascript.png';
-import ReactImg from '../public/assets/skills/react.png';
-import Tailwind from '../public/assets/skills/tailwind.png';
-import Github from '../public/assets/skills/github1.png';
-import Firebase from '../public/assets/skills/firebase.png';
-import NextJS from '../public/assets/skills/nextjs.png'
-import AWS from '../public/assets/skills/aws.png';
+{/* Import skill icons */}
+import Image from "next/image";
+import React from "react";
+import Html from "../public/assets/skills/html.png";
+import Css from "../public/assets/skills/css.png";
+import Javascript from "../public/assets/skills/javascript.png";
+import ReactImg from "../public/assets/skills/react.png";
+import Tailwind from "../public/assets/skills/tailwind.png";
+import Github from "../public/assets/skills/github1.png";
+import Firebase from "../public/assets/skills/firebase.png";
+import NextJS from "../public/assets/skills/nextjs.png";
+import AWS from "../public/assets/skills/aws.png";
+import Redux from "../public/assets/skills/redux.svg";
 
+
+{/* Creating skill cards*/}
 const Skills = () => {
   return (
     <div id="skills" className="w-full lg:h-screen p-2">
@@ -112,10 +116,10 @@ const Skills = () => {
           <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
-                <Image src={AWS} width="64px" height="64px" alt="/" />
+                <Image src={Redux} width="64px" height="64px" alt="/" />
               </div>
               <div className="flex flex-col items-center justify-center">
-                <h3>AWS</h3>
+                <h3>Redux</h3>
               </div>
             </div>
           </div>
