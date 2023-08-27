1a083a19516dd061a89d80d32e3c83eff999ba38
jpromanonet <programming@jpromano.net>
2023-08-26 21:20:27 -0300

    New description

--- pages/resume.jsx
+++ pages/resume.jsx
@@ -38,7 +38,7 @@ const resume = () => {
         <div className="text-center py-4 text-xl font-bold uppercase tracking-wider">
           <div className="hidden sm:block">
             <p>
-              IT Manager <span className="px-1"></span>{" "}
+              Engineering Manager <span className="px-1"></span>{" "}
             </p>
           </div>
         </div>
